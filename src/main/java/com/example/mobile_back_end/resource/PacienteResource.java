5e96575e42b2174eb0ed68fc6b6248809a5515de
Igor Queiroz <igorczz.10@gmail.com>
2024-10-08 16:57:37 -0300

    mudança para verificar se o msyl esta ativo no paciente

--- src/main/java/com/example/mobile_back_end/resource/PacienteResource.java
+++ src/main/java/com/example/mobile_back_end/resource/PacienteResource.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,11 @@ public class PacienteResource implements GenericOperations<Paciente, Integer> {
 
     @Autowired
     private PacienteService pacienteService;
+
+    @GetMapping("/ping")
+    public boolean ping() {
+        return true;
+    }
 
 
     @PostMapping(
